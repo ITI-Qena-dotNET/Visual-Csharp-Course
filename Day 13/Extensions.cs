861b8cf0786122b04e2a99ec16460f8b90ac906b
Youssef Wael Mohamed-Lotfy Mousa <40206862+YoussefWaelMohamedLotfy@users.noreply.github.com>
2023-02-08 14:10:41 +0200

    Add Lab 13 Task Solution

---
+++ Day 13/Extensions.cs
@@ -0,0 +1,90 @@
+﻿using System.Text;
+
+namespace Lab13;
+
+public static class Extensions
+{
+    public static int Reverse(this int num)
+    {
+        string numString = num.ToString();
+        var reversedEnumerable = numString.Reverse();
+
+        StringBuilder result = new();
+
+        foreach (var item in reversedEnumerable)
+        {
+            result.Append(item);
+        }
+
+        return int.Parse(result.ToString());
+    }
+
+    public static int NumberOfDigits(this int num)
+    {
+        //return num.ToString().Length;
+
+        int counter = 0;
+
+        string numString = num.ToString();
+
+        foreach (var item in numString)
+        {
+            counter++;
+        }
+
+        return counter;
+    }
+
+    public static string RemoveSpecialCharacters(this string text)
+    {
+        StringBuilder result = new();
+
+        foreach (char c in text)
+        {
+            switch (c)
+            {
+                case '-':
+                case '+':
+                case '/':
+                case '_':
+                case '*':
+                    break;
+                default: 
+                    result.Append(c); 
+                    break;
+            }
+        }
+
+        return result.ToString();
+    }
+
+    public static int GetMaxInCollection(this IEnumerable<int> intCollection)
+    {
+        //return intCollection.Max();
+
+        int currentMax = int.MinValue;
+
+        foreach (int i in intCollection)
+        {
+            if (i > currentMax) 
+                currentMax = i;
+        }
+
+        return currentMax;
+    }
+
+    public static T GetMaxInCollection<T>(this IEnumerable<T> collection)
+    {
+        return collection.Max();
+
+        //var currentMax = ;
+
+        //foreach (var i in collection)
+        //{
+        //    if (i > currentMax)
+        //        currentMax = i;
+        //}
+
+        //return currentMax;
+    }
+}
---
+++ Day 13/Program.cs
@@ -0,0 +1,14 @@
+﻿using Lab13;
+
+int number = 1423;
+
+Console.WriteLine($"Number {number} reversed is: {number.Reverse()}");
+Console.WriteLine($"Number of digits in {number} is: {number.NumberOfDigits()}");
+
+string text = "H-e+l_l*o c_#";
+Console.WriteLine($"Text {text} after removing special chars is {text.RemoveSpecialCharacters()}");
+
+IEnumerable<int> numbers = new List<int>() { 3, 4, 18, 5, 6 };
+Console.WriteLine($"Max number of list is {numbers.GetMaxInCollection()}");
+
+Console.WriteLine($"Max of text list is {text.GetMaxInCollection()}");
